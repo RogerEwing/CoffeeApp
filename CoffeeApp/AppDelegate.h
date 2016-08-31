eaeafecd86056d2d61ae901bc13e7f48ca395cd5
Matthew Ewing <m_r_ewing@hotmail.com>
2016-08-31 09:11:15 +0100

    Initial Commit

---
+++ CoffeeApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CoffeeApp
+//
+//  Created by User on 31/08/2016.
+//  Copyright © 2016 RogersDesigns. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CoffeeApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CoffeeApp
+//
+//  Created by User on 31/08/2016.
+//  Copyright © 2016 RogersDesigns. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
